abd5e8e06f0c9a264c6c18ce8231b74f64b75337
SonoSooS <robonyunyu@gmail.com>
2019-09-14 15:10:45 +0200

    Add Linux path support to lz.c include

--- soos/lz.c
+++ soos/lz.c
@@ -1,1 +1,10 @@
+//old shit
+#if 1
+#ifdef _WIN32
 #include "D:\\Data\\lolol\\3DS\\TWL\\twltest\\lz.c"
+#else
+#include "/media/Wasteland/Data/lolol/3DS/TWL/twltest/lz.c"
+#endif
+#else
+#error lz.c rewrite is not done yet
+#endif
